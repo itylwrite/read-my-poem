880ab486c3c0b95fa6572d56d1805a3633b30162
itylwrite <itylwrite@gmail.com>
2026-04-28 00:50:20 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
-const _supabaseUrl = 'https://sguwqcgonjnjblgxlun.supabase.co';
+const _supabaseUrl = 'https://sguwqcgonjnjjblgxlun.supabase.co';
 const _supabaseAnonKey = 'sb_publishable_Myy5VECF9_qmJtN2cqhGfg_YVnKwgYH';
 const db = supabase.createClient(_supabaseUrl, _supabaseAnonKey);
 
