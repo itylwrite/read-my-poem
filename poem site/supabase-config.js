7cb0e6843dc50c1f23ad80c4ec6b9f65e8adeb17
itylwrite <itylwrite@gmail.com>
2026-04-27 17:36:45 +0800

    Delete poem site/supabase-config.js

--- poem site/supabase-config.js
+++
@@ -1,3 +0,0 @@
-const _supabaseUrl = 'https://oxtoyipoyorixmzgbtzl.supabase.co/rest/v1/';
-const _supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dG95aXBveW9yaXhtemdidHpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxODczNjAsImV4cCI6MjA5Mjc2MzM2MH0.9KLP7123V13KLtM-uALnc4loHswoF-uI0HR5TPjYX0U';
-const db = supabase.createClient(_supabaseUrl, _supabaseAnonKey);
