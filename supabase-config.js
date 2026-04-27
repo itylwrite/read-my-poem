31d05b5575120b53ff3c29b76fd45dea32edeadf
itylwrite <itylwrite@gmail.com>
2026-04-28 00:21:53 +0800

    Update Supabase configuration with new URL and key

--- supabase-config.js
+++ supabase-config.js
@@ -1,3 +1,5 @@
-const _supabaseUrl = 'https://oxtoyipoyorixmzgbtzl.supabase.co/rest/v1/';
-const _supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94dG95aXBveW9yaXhtemdidHpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxODczNjAsImV4cCI6MjA5Mjc2MzM2MH0.9KLP7123V13KLtM-uALnc4loHswoF-uI0HR5TPjYX0U';
+const _supabaseUrl = 'https://sguwqcgonjnjblgxlun.supabase.co';
+const _supabaseAnonKey = 'sb_publishable_Myy5VECF9_qmJtN2cqhGfg_YVnKwgYH';
 const db = supabase.createClient(_supabaseUrl, _supabaseAnonKey);
+
+
